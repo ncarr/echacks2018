d5df2d61e68037757d01f1c96158d54b5cec1d19
Emily Goose <EmilyGoose@users.noreply.github.com>
2018-11-03 00:28:41 -0400

    Added fingerings

--- PythonFiles/midotest.py
+++ PythonFiles/midotest.py
@@ -1,13 +1,19 @@
 # Library imports
 import time
 import mido
+import json
 
 # Get the midi file
 songFile = mido.MidiFile('mad_world.mid')
 
 # For reference = Bb is note 46, High Bb is 58, Middle C is 60
 
+# Load the fingerings file for the baritone
+with open('fingerings.json') as json_file:
+    fingerings = json.load(json_file)
+
 # Iterate over messages in it
 for msg in songFile:
-    time.sleep(msg.time)
-    print(msg)
+    if msg.type == 'note_on' and msg.velocity == 0:
+        time.sleep(msg.time)
+        print(fingerings[str(msg.note)])
