6197315e395f09ad1e3aefd6a1a7aaf48885995e
Emily Goose <EmilyGoose@users.noreply.github.com>
2018-11-03 12:39:48 -0400

    Oh no merge conflict incoming

--- PythonFiles/server.py
+++ PythonFiles/server.py
@@ -1,5 +1,4 @@
 # Library imports
-import time
 import mido
 import json
 import asyncio
@@ -9,6 +8,14 @@ import websockets
 songFile = mido.MidiFile('mad_world.mid')
 sockets = set()
 valves = ''
+
+# Convert the midi file into a JSON that we can send to the Vue server via socket
+midiList = []
+for msg in songFile:
+    if msg.type == 'note_on' and msg.velocity == 0:
+        midiList.append(msg)
+# Convert it to JSON
+midiJSON = json.dumps(midiList)
 
 # For reference = Bb is note 46, High Bb is 58, Middle C is 60
 # Load the fingerings file for the baritone
@@ -30,9 +37,10 @@ async def iterate_over_file(midi_file):
             global expected
             expected = fingerings[str(msg.note)]
             await asyncio.sleep(msg.time)
-            # Calculate the accuracy for the last note
             try:
+                # Calculate the accuracy for the previous note
                 accuracy_percentage = sum(currentNoteAccuracy) / len(currentNoteAccuracy)
+                # Add it to the list of the whole song
                 songAccuracy.append(accuracy_percentage)
                 currentNoteAccuracy.clear()
             except ZeroDivisionError:
@@ -47,17 +55,16 @@ async def counter(websocket, path):
         async for message in websocket:
             global valves
             valves = message
-            print("Current: " + valves)
-            print("Expected: " + expected)
-            print("Active: " + str(songActive))
             if songActive:
+                print("Current: " + valves)
+                print("Expected: " + expected)
+                # Log whether the player hit the note
                 if valves == expected:
                     currentNoteAccuracy.append(1)
                 else:
                     currentNoteAccuracy.append(0)
     except Exception as e:
         print(e)
-
 
 asyncio.get_event_loop().run_until_complete(
     websockets.serve(counter, '0.0.0.0', 6789))
