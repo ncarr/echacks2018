1a018bc797bef20292891053f08da44fe69e1e6a
Jonathan <jonathanxu25@gmail.com>
2018-11-03 14:23:43 -0400

    reeee also button changes color now

--- frontend/src/components/Valves.vue
+++ frontend/src/components/Valves.vue
@@ -18,31 +18,68 @@
 <div id="column">
     <h1 class="headline">Valves</h1>
     <div id="row">
-        <v-btn fab dark large color="purple"></v-btn>
-        <v-btn fab dark large color="purple"></v-btn>
-        <v-btn fab dark large color="purple"></v-btn>
+        <v-btn fab dark large :color="vc1">1</v-btn>
+        <v-btn fab dark large :color="vc2">2</v-btn>
+        <v-btn fab dark large :color="vc3">3</v-btn>
     </div>
 </div>
 
 </template>
 
 <script>
-  var ws = new WebSocket("ws://192.168.9.28:6789");
+  export default {
+    name: 'Valves',
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        vc1 : "purple",
+        vc2 : "purple",
+        vc3 : "purple"
+      }
+    },
+    mounted() {
+      var scope = this;
+      var ws = new WebSocket("ws://192.168.9.28:6789");
 
-  ws.onopen = function() {
-    console.log("Connection started...");
-  };
+      ws.onopen = function() {
+        //ws.send("0 1 1");
+        ws.send("ping");
+        console.log("Connection started...");
+      };
 
-  ws.onmessage = function(evt) {
-      var received_msg = evt.data;
-      var v1 = parseInt(received_msg.substring(0, 1));
-      var v2 = parseInt(received_msg.substring(2, 3));
-      var v3 = parseInt(received_msg.substring(4, 5));
-      console.log(v1.toString() + " " + v2.toString() + " " + v2.toString());
-  };
+      ws.onmessage = function(evt) {
+          var received_msg = evt.data;
+          var v1 = parseInt(received_msg.substring(0, 1));
+          var v2 = parseInt(received_msg.substring(2, 3));
+          var v3 = parseInt(received_msg.substring(4, 5));
+          console.log(v1.toString() + " " + v2.toString() + " " + v3.toString());
 
-  ws.onclose = function() {
-      // websocket is closed.
-      console.log("Connection is closed...");
-  };
+          if(v1==1){
+            scope.vc1 = "blue";
+          } else {
+            scope.vc1 = "purple";
+          }
+
+          if(v2==1){
+            scope.vc2 = "blue";
+          } else {
+            scope.vc2 = "purple";
+          }
+
+          if(v3==1){
+            scope.vc3 = "blue";
+          } else {
+            scope.vc3 = "purple";
+          }
+      };
+
+      ws.onclose = function() {
+          // websocket is closed.
+          console.log("Connection is closed...");
+      };
+    }
+  }
+
 </script>
