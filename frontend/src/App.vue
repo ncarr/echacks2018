dba9b711ebb238aaaea744ec5aff766d9a9fa4bb
Jonathan <jonathanxu25@gmail.com>
2018-11-03 11:54:56 -0400

    change to valve component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>ECHacks</span>
+        <span class="font-weight-light">PROJECT NAME</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
@@ -16,18 +16,18 @@
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <Valves/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Valves from './components/Valves'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Valves
   },
   data () {
     return {
