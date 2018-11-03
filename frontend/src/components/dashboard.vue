0ec784dd80ad5b9edd4be45ac80323e518df542d
MansonHau <34991227+MansonHau@users.noreply.github.com>
2018-11-03 12:56:39 -0400

    Dashboard

---
+++ frontend/src/components/dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  <h1 class="headline">Dashboard</h1>
+  <p> Welcome to the Sensor Baritone </p>
+  <button type="button"> Enter Name and Music </button>
+  </div>
+
+</template>
+
+
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+button {
+    background-color: #4CAF50; /* Green background */
+    border: 1px solid green; /* Green border */
+    color: white; /* White text */
+    padding: 10px 24px; /* Some padding */
+    cursor: pointer; /* Pointer/hand icon */
+    float: left; /* Float the buttons side by side */
+}
+
+  
+</style>
