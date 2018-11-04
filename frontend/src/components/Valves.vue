1a6338daaac5a41e73d298983db005184e0fe0da
Nicholas Carr <ncarr@users.noreply.github.com>
2018-11-04 10:51:11 -0500

    eXPECTED VALVES

--- frontend/src/components/Valves.vue
+++ frontend/src/components/Valves.vue
@@ -1,13 +1,14 @@
 <template>
 
 <div id="column">
-    <h1 class="headline">Valves</h1>
+    <h1 class="headline" v-if="expected">Expected</h1>
+    <h1 class="headline" v-else>Valves</h1>
     <v-layout row wrap>
         <v-btn fab dark large :color="valve1 ? 'blue': 'purple'">1</v-btn>
         <v-btn fab dark large :color="valve2 ? 'blue': 'purple'">2</v-btn>
         <v-btn fab dark large :color="valve3 ? 'blue': 'purple'">3</v-btn>
     </v-layout>
-    <v-layout row>
+    <v-layout row v-if="!expected">
       <v-btn dark :color="volume ? 'blue' : 'purple'">Now playing</v-btn>
     </v-layout>
 </div>
@@ -21,7 +22,8 @@
       valve1: Boolean,
       valve2: Boolean,
       valve3: Boolean,
-      volume: Boolean
+      volume: Boolean,
+      expected: Boolean
     }
   }
 
